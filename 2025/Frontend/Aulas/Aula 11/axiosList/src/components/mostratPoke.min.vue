ea5f4207371c03d84dc1837f2cea688b75c80589
Enzo Ayres Bettini <enzobettini@hotmail.com>
2025-05-29 21:25:45 -0300

    fim aula 29/05/2025

---
+++ 2025/Frontend/Aulas/Aula 11/axiosList/src/components/mostratPoke.min.vue
@@ -0,0 +1,1 @@
+<script setup>const props=defineProps(['nome', 'link']);</script><template><li>{{ nome}} ---{{ link}} </li></template>
---
+++ 2025/Frontend/Aulas/Aula 11/axiosList/src/components/mostratPoke.vue
@@ -0,0 +1,6 @@
+<script setup>
+const props = defineProps(['nome', 'link']);
+</script>
+<template>
+    <li> {{ nome }} --- {{ link }} </li>
+</template>
